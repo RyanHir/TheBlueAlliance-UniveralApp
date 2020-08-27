ea41fde18944cd9561568b5b3ef1633f9642c8c6
Ryan Hirasaki <4690732+RyanHir@users.noreply.github.com>
2020-08-27 15:11:34 -0500

    Delete old code and add empty files for later containers

--- lib/UI/Views/Home.dart
+++ lib/UI/Views/Home.dart
@@ -1,36 +1,51 @@
 import 'package:flutter/material.dart';
 import 'package:tba/constants.dart';
 
-class Home extends StatelessWidget {
+class Home extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return HomeState();
+  }
+}
+
+class HomeState extends State<StatefulWidget> {
+  // ignore: unused_field
+  int _position = 0;
+
   @override
   Widget build(BuildContext context) {
+    
     return new Scaffold(
       appBar: AppBar(title: Text(kAppNameLong), actions: [
         IconButton(
-          icon: Icon(Icons.settings), 
-          onPressed: () => Navigator.pushNamed(context, "/settings"))
+            icon: Icon(Icons.settings),
+            onPressed: () => Navigator.pushNamed(context, "/settings"))
       ]),
       body: Center(
-        child: Text("Test"),
+        child: Text("Position: $_position"),
       ),
-      bottomNavigationBar: new BottomNavigationBar(items: [
-        BottomNavigationBarItem(
-          icon: Icon(Icons.calendar_today),
-          title: Text("Events"),
-        ),
-        BottomNavigationBarItem(
-          icon: Icon(Icons.people),
-          title: Text("Teams"),
-        ),
-        BottomNavigationBarItem(
-          icon: Icon(Icons.group_work),
-          title: Text("Districts"),
-        ),
-        BottomNavigationBarItem(
-          icon: Icon(Icons.star),
-          title: Text("myTBA"),
-        ),
-      ]),
+      bottomNavigationBar: new BottomNavigationBar(
+        items: [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.calendar_today),
+            title: Text("Events"),
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.people),
+            title: Text("Teams"),
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.group_work),
+            title: Text("Districts"),
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.star),
+            title: Text("myTBA"),
+          ),
+        ],
+        onTap: (position) => setState(() => _position = position),
+        currentIndex: _position,
+      ),
     );
   }
 }
