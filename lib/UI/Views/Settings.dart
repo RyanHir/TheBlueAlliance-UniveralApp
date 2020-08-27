fb72e195f6f92ae6c6f049272ac1884f74a2391a
Ryan Hirasaki <4690732+RyanHir@users.noreply.github.com>
2020-08-27 14:59:32 -0500

    Add login button

--- lib/UI/Views/Settings.dart
+++ lib/UI/Views/Settings.dart
@@ -5,6 +5,15 @@ class Settings extends StatelessWidget {
   Widget build(BuildContext context) {
     return new Scaffold(
       appBar: AppBar(title: Text("Settings")),
+      body: ListView(
+        children: [
+          ListTile(
+            title: Text("Login to myTBA"),
+            trailing: Icon(Icons.arrow_forward),
+            onTap: () => Navigator.pushNamed(context, "/settings/login"),
+          )
+        ],
+      )
     );
   }
 }
