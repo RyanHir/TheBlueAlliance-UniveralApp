3cff6379bbe7f0259b93db1c5284df93006ac993
Ryan Hirasaki <4690732+RyanHir@users.noreply.github.com>
2020-08-27 17:48:16 -0500

    Extremly basic event list

--- lib/UI/Views/Menus/Events.dart
+++ lib/UI/Views/Menus/Events.dart
@@ -1,8 +1,42 @@
 import 'package:flutter/material.dart';
+import 'package:tba/Tools/apiFetch.dart';
 
-class Events extends StatelessWidget {
+class Events extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return EventState();
+  }
+}
+
+class EventState extends State<StatefulWidget> {
+  bool _firstLoad = true;
+  bool _loading = true;
+  var events = [];
+
+  void _fetch() async {
+    events = await fetch("events/2020", {});
+    setState(() => _loading = false);
+  }
+
   @override
   Widget build(BuildContext context) {
-    throw UnimplementedError();
+    if (_firstLoad) {
+      _fetch();
+      _firstLoad = false;
+    }
+    if (_loading) {
+      return Center(
+        child: CircularProgressIndicator(),
+      );
+    }
+    return Center(
+        child: ListView.builder(
+            itemCount: events.length,
+            itemBuilder: (context, item) {
+              return ListTile(
+                title: Text(events[item]["name"]),
+                trailing: Icon(Icons.arrow_forward),
+              );
+            }));
   }
 }
