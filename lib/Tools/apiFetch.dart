cfba217daabf30e2a7d307a80cedd6d2538103b1
Ryan Hirasaki <4690732+RyanHir@users.noreply.github.com>
2020-08-28 15:23:19 -0500

    Sucessful fetching from api

--- lib/Tools/apiFetch.dart
+++ lib/Tools/apiFetch.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/widgets.dart';
 import 'package:http/http.dart' as http;
 import 'package:shared_preferences/shared_preferences.dart';
 import 'dart:convert' as decode;
@@ -14,41 +15,28 @@ const _baseHeader = {
 
 Future<dynamic> fetch(String endpoint, Map<String, String> extra) async {
   // Keys
-  final String cacheKey = "/$endpoint/cache";
-  final String lastModKey = "/$endpoint/last_mod";
+  final String cacheKey = "/cache/$endpoint/data";
+  final String lastModKey = "/cache/$endpoint/last_mod";
+  final String url = "$_baseURL/$endpoint";
+  final SharedPreferences prefs = await SharedPreferences.getInstance();
+  final Map<String, String> header = {..._baseHeader, ...extra};
 
-  SharedPreferences prefs = await SharedPreferences.getInstance();
+  final response = await http.get(url,
+      headers: Map.from(header)
+        ..addAll({"If-Modified-Since": prefs.getString(lastModKey) ?? ""}));
 
-  var header = _baseHeader;
-  var url = "$_baseURL/$endpoint";
-  extra.forEach((k, v) => header[k] = v);
-
-  if (prefs.containsKey(lastModKey)) {
-    try {
-      String modDate = prefs.getString(lastModKey);
-      header["If-Modified-Since"] = modDate;
-      print("Found Date in cache");
-    } catch (e) {
-      print(e);
-    }
-  } else {
-    print("Missing Date in cache");
-  }
-
-  http.Response response = await http.get(url, headers: header);
-
-  print("Response Code: ${response.statusCode}");
-  if (response.statusCode == 200) {
-    assert(response.headers.containsKey("last-modified"));
-    await prefs.setString(cacheKey, response.body);
-    await prefs.setString(lastModKey, response.headers["last-modified"]);
-    return decode.jsonDecode(response.body);
-  } else if (response.statusCode == 304) {
-    print("Pulling from Cache");
-    assert(prefs.containsKey(cacheKey));
-    assert(prefs.containsKey(lastModKey));
-    return decode.jsonDecode(prefs.getString(cacheKey));
-  } else {
-    throw UnimplementedError();
+  switch (response.statusCode) {
+    case 200:
+      await Future.wait([
+        prefs.setString(cacheKey, response.body),
+        prefs.setString(lastModKey, response.headers["last-modified"])
+      ]);
+      return decode.jsonDecode(response.body);
+    case 304:
+      debugPrint("Using Data from cache");
+      return decode.jsonDecode(prefs.getString(cacheKey));
+    default:
+      print("Unknown Response Code: ${response.statusCode}");
+      throw UnimplementedError();
   }
 }
